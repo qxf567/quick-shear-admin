894db066022f55e4010adb34855ded7e69357c3c
denglongshan <denglongshan@lashou-inc.com>
2017-02-09 10:42:26 +0800

    init

--- src/main/java/com/shear/admin/controller/login/LoginController.java
+++ src/main/java/com/shear/admin/controller/login/LoginController.java
@@ -1,20 +1,26 @@
 package com.shear.admin.controller.login;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.quickshear.common.util.JsonUtil;
 import com.quickshear.common.util.RetdCodeType;
 import com.quickshear.common.vo.ResObj;
+import com.quickshear.common.wechat.WechatUserInfoManager;
 import com.quickshear.domain.User;
 import com.quickshear.domain.query.UserQuery;
 import com.quickshear.service.UserService;
@@ -32,20 +38,37 @@ public class LoginController extends AbstractController {
     private UserService userService;
     @Autowired
     private StorageService storageService;
+    @Autowired
+    private WechatUserInfoManager wechatUserInfoManager;
 
-    @RequestMapping(value = "/login")
-    public String login(Model model, HttpServletRequest request) {
-	String openid = storageService.get(request, "openid");
-	UserQuery queryObj = new UserQuery();
-	queryObj.setWechatOpenId(openid);
-	List<User> userList = null;
+    @RequestMapping(value = "/login/{openid}")
+    public String login(Model model,HttpServletResponse response, @PathVariable(value = "openid") String openid) {
 	try {
+	    //存cookie
+	    storageService.set("openid", openid, response);
+	    LOGGER.info("login openid" + openid);
+	    //查询用户是否存在
+	    UserQuery queryObj = new UserQuery();
+	    queryObj.setWechatOpenId(openid);
+	    List<User> userList = null;
 	    userList = userService.selectByParam(queryObj);
 	    if (userList == null || userList.size() == 0) {
 		model.addAttribute("isNewUser", "1");
-		model.addAttribute("message", "您还不是系统用户,请提交资料进行审核："+openid);
+		model.addAttribute("message", "您还不是系统用户,请提交资料进行审核：");
 		return "register";
 	    }
+	    //获取用户信息
+	    Map<String, Object> wechatUserInfo = wechatUserInfoManager
+		    .getWechatUserInfoByPageAccess(openid);
+	    Map<String, Object> userInfo = new HashMap<String, Object>();
+	    if (wechatUserInfo != null) {
+		userInfo.put("nickname", wechatUserInfo.get("nickname"));
+		userInfo.put("headimgurl", wechatUserInfo.get("headimgurl"));
+		LOGGER.info("wechatUserInfo:"+wechatUserInfo);
+	    }
+	    //存cookie
+	    storageService.set("userInfo", JsonUtil.toJson(userInfo), response);
+	    //页面跳转
 	    String roles = userList.get(0).getRoles();
 	    if ("1".equals(roles)) {// 管理员
 		return "admin/index";
@@ -73,6 +96,12 @@ public class LoginController extends AbstractController {
 	resObj.getMessage().setMsg(RetdCodeType.PASS_OK.getMsg());
 	try {
 	    String openid = storageService.get(request, "openid");
+	    LOGGER.info("openid" + openid);
+	    if (openid == null) {
+		resObj.setCode(RetdCodeType.EX_APP.getCode());
+		resObj.getMessage().setMsg(RetdCodeType.EX_APP.getMsg());
+		return resObj;
+	    }
 	    User user=new User();
 	    user.setWechatOpenId(openid);
 	    user.setRoles("300");
